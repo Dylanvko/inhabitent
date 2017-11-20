3c420c70ad426882d551bcc3d1b9d010c8bbec62
Dylan Kocsis <Dylan.Kocsis@queensu.ca>
2017-11-19 16:52:55 -0800

    finished styling on 'find us' page.

--- themes/inhabitent/template-parts/content-page.php
+++ themes/inhabitent/template-parts/content-page.php
@@ -11,9 +11,15 @@
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.6833050933724!2d-123.14036234872295!3d49.26344827922775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673c79e1ac457%3A0x3aea6428fa30dc6a!2s1490+W+Broadway%2C+Vancouver%2C+BC+V6H!5e0!3m2!1sen!2sca!4v1511138840388" width="100%" height="300px" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 	<div class="entry-content">
+	<h2>We take camping very seriously.</h2>
+	<p>Inhabitent Camping Supply Co. knows what it takes to outfit a camping trip right. From flannel shirts to artisanal axes, we’ve got your covered. Please contact us below with any questions comments or suggestions.</p>
+	<h2>Send us email!</h2>
+
 		<?php the_content(); ?>
+
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
